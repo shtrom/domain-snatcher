4c38a1e1264bd8e8fafeff2fdb989879b4af61d3
Olivier Mehani <shtrom@ssji.net>
2021-09-09 23:36:00 +1000

    Copy and refine credentials.js from [0]
    Use with
    
        docker run -it --rm --env-file .env -w /srv -v /home/shtrom/src/domain-snatcher:/srv node:14 node credentials.js
    
    and don't forget to follow the validation link.
    
    [0] https://github.com/ovh/node-ovh\#example-usage
    
    Signed-off-by: Olivier Mehani <shtrom@ssji.net>

---
+++ credentials.js
@@ -0,0 +1,16 @@
+var ovh = require('ovh')({
+  endpoint: 'ovh-eu',
+  appKey: process.env.APP_KEY,
+  appSecret: process.env.APP_SECRET
+});
+
+ovh.request('POST', '/auth/credential', {
+  'accessRules': [
+    { 'method': 'GET', 'path': '/order/cart/*'},
+    { 'method': 'GET', 'path': '/me/order/*'},
+    { 'method': 'POST', 'path': '/order/cart/*'},
+    { 'method': 'DELETE', 'path': '/order/cart/*'}
+  ]
+}, function (error, credential) {
+  console.log(error || credential);
+});
